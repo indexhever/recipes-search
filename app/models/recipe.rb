93127c89cedab888b2940cdc8cabf04c97939fd6
heverton <heverton.sarah@gmail.com>
2017-02-12 20:38:26 -0300

    fixing json bug

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,8 @@ class Recipe
 	include HTTParty
 
 	base_uri 'http://food2fork.com/api/search'
-	default_params key: ENV["FOOD2FORK_KEY"]
+	#default_params key: ENV["FOOD2FORK_KEY"]
+	default_params key: "0fdaa56c3e245939e57ec30d27a2ed37"
 	format :json
 
 	def self.for term
