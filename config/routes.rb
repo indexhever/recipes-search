b4703d797a756ac489720638ad1664d87b3c069c
heverton <heverton.sarah@gmail.com>
2017-02-12 20:15:15 -0300

    Adding recipes imgs and links to recipes pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'courses/index'
+  get 'recipes/index'
 
   get 'greeter/hi' => "greeter#hello"
   get 'greeter/goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'courses#index'
+  root 'recipes#index'
 end
