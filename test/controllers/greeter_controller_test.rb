38e7fb0c0bc1c63edbc4e9f1cae655447f86cfde
heverton <heverton.sarah@gmail.com>
2017-02-14 10:52:40 -0300

    Deleting trash files

--- test/controllers/greeter_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GreeterControllerTest < ActionDispatch::IntegrationTest
-  test "should get hello" do
-    get greeter_hello_url
-    assert_response :success
-  end
-
-end
