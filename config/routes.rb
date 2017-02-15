d0e36a6eba3ce22ae41a88f15b4822398eae9e93
heverton <heverton.sarah@gmail.com>
2017-02-15 14:43:26 -0300

    Adding css style, changin home page, adding meny bar

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'recipes#index'
-  #root 'static_pages#home'
+  #root 'recipes#index'
+  root 'static_pages#home'
   #root 'users#index'
 end
