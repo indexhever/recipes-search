e848b1a7312f128df4f5b318082779c3466450f3
heverton <heverton.sarah@gmail.com>
2017-02-15 21:47:38 -0300

    Adding links to menu and more tests for new pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
+  root 'static_pages#home'
 
-  get 'static_pages/about'
+  get 'help' => 'static_pages#help'
 
-  get 'static_pages/contact'
+  get 'about' => 'static_pages#about'
 
-  resources :microposts
-  resources :users
+  get 'contact' => 'static_pages#contact'
+
+  get 'signup' => 'users#new'
+
+  #resources :microposts
+  #resources :users
   get 'recipes/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #root 'recipes#index'
-  root 'static_pages#home'
+  
   #root 'users#index'
 end
