c8281d21924a2443359459e46301c578aee2a96c
heverton <heverton.sarah@gmail.com>
2017-02-18 22:49:35 -0300

    users signup finalization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   	@user = User.new(user_params) # Not the final implementation!
 	if @user.save
 	# Handle a successful save.
+		flash[:success] = "Welcome to the Fooresh!"
+		redirect_to @user
 	else
 		render 'new'
 	end
