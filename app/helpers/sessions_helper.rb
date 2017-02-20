59fe79a8e9ec85952ab7a67f0f06bff4617af571
heverton <heverton.sarah@gmail.com>
2017-02-20 14:51:36 -0300

    fixing logout bug

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -42,10 +42,4 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
-
-	# Logs out the current user.
-	def log_out
-		session.delete(:user_id)
-		@current_user = nil
-	end
 end
