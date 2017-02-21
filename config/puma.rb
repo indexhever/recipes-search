15876b6e626c3b513f8be6957b3f5b066da08748
heverton <heverton.sarah@gmail.com>
2017-02-21 11:31:18 -0300

    Adding update user functionality and its tests

--- config/puma.rb
+++ config/puma.rb
@@ -15,8 +15,8 @@
 #
 #environment ENV.fetch("RAILS_ENV") { "development" }
 
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+#threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+#threads threads_count, threads_count
 
 preload_app!
 
