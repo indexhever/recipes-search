86af6537d538f32532d46c5acba310c122897ce7
heverton <heverton.sarah@gmail.com>
2017-02-22 03:11:12 -0300

    Restricting access to user profiles to only logged ones

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -7,10 +7,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
   	get signup_path
   	assert_no_difference 'User.count' do
-  		post users_path, user: { name: "",
+  		post users_path, params: { user: { name: "",
   								 email: "user@invalid",
   								 password:     			"foo",
-  								 password_confirmation: "bar"}
+  								 password_confirmation: "bar"} }
   	end
   	assert_template 'users/new'	  	
     assert_select 'div#error_explanation'
@@ -20,10 +20,11 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "valid signup information" do
   	get signup_path
   	assert_difference 'User.count', 1 do
-  		post_via_redirect users_path, user: { name: "Example User",
+  		post users_path, params: { user: { name: "Example User",
   									  email: "user@example.com",
   									  password: 			"password",
-  									  password_confirmation: "password" }
+  									  password_confirmation: "password" } }
+      follow_redirect!
     end
     assert_template 'users/show'
     assert_not flash.nil?
