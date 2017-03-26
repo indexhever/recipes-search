46cf46107e477fb707489744c685ceee76c8ee9a
heverton <heverton.sarah@gmail.com>
2017-03-25 23:28:50 -0300

    Adding micropost with image upload integration tests

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 		log_in_as(@user)
 		get root_path
 		assert_select 'div.pagination'
+		assert_select 'input[type="submit"]'
 		# invalid submission
 		assert_no_difference 'Micropost.count' do
 			post microposts_path, params: { micropost: { content: "" } }
@@ -16,9 +17,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 		assert_select 'div#error_explanation'
 		# valid submission
 		content = "This micropost really ties the room together"
-		assert_difference 'Micropost.count' do
-			post microposts_path, params: { micropost: { content: content } }
+		picture = fixture_file_upload('test/fixtures/rails.jpg', 'image/jpeg', :binary)
+		assert_difference 'Micropost.count', 1 do
+			post microposts_path, params: { micropost: { content: content, picture: picture } }
 		end
+		#assert FILL_IN.picture? #see latter
 		assert_redirected_to root_url
 		follow_redirect!
 		assert_match content, response.body
